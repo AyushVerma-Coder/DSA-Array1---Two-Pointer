b00592f6d41450c2e216ee55c999f626361db05f
AyushVerma-Coder <ayush20303@gmail.com>
2026-04-10 16:50:21 +0530

    Two pointer approach
    //Two Sum addition of any two digits in an array
    //A={2,5,7,8,12}
    //Sum=15
    //A is sorted.

---
+++ Arrays3.java
@@ -0,0 +1,34 @@
+package DSA;
+//Two Sum addition of any two digits in an array
+//A={2,5,7,8,12}
+//Sum=15
+//A is sorted.
+
+import java.util.Arrays;
+//Two Pointer Solution. - Optimal Solution
+public class Arrays3 {
+
+	public static void main(String[] args) {
+		int arr[]= {2,5,7,8,12};
+		int sum=15;
+		int i=0;
+		int j=arr.length-1;
+		while (i<j) {
+			
+			if (arr[i]+arr[j]==sum) {
+				System.out.println("Pair found: " + arr[i] + ", " + arr[j]);
+				break;
+			}
+			else if((arr[i]+arr[j])<sum) {
+				i++;
+			}
+			else {
+				j--;
+			}
+		}
+		
+
+	}
+
+}
+
